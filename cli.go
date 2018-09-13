d872bf88532e216464551e167d05b3a5ee747640
Raül Pérez <repejota@gmail.com>
2018-09-13 18:03:28 +0200

    Command CLI types are CommandInterface implementations

---
+++ cli.go
@@ -0,0 +1,51 @@
+// Copyright 2018, Goomba project Authors. All rights reserved.
+//
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with this
+// work for additional information regarding copyright ownership.  The ASF
+// licenses this file to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package cli
+
+import "fmt"
+
+// CommandInterface ...
+type CommandInterface interface {
+	Run(cmd *Command, args []string)
+}
+
+// CLI is the main entry point of a CLI application
+type CLI struct {
+	ProgramName string
+	Commands    []*CommandInterface
+}
+
+// NewCLI ...
+func NewCLI() *CLI {
+	cli := &CLI{
+		ProgramName: "",
+		Commands:    make([]*CommandInterface, 0),
+	}
+
+	return cli
+}
+
+// AddCommand ...
+func (c *CLI) AddCommand(command *CommandInterface) {
+	c.Commands = append(c.Commands, command)
+}
+
+// Run ...
+func (c *CLI) Run(args []string) {
+	fmt.Printf("Run %s with args %s", c.ProgramName, args)
+}
